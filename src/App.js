d566c2a829af644ef9a46cd2f76e5a5806781241
Vinay Singhal <vinaysinghal50@gmail.com>
2021-03-04 23:39:39 +0530

    navigation Bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
